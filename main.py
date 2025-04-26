e2f50b6cb99c3118f89bcf8eca30305f4f41fbb1
Voldemort0702 <tommy.870702@gmail.com>
2025-04-26 10:43:09 +0800

    update env

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # ====== SETUP ======
-TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiJhd3NfaGFja2F0aG9uIiwiZXhwaXJlcyI6MTc0NTc0ODAwMH0.9qpg1xraE_d_Hua2brAmCfRlQSce6p2kdipgq8j1iqo'  # <-- Replace with your real Bearer Token
+TOKEN = os.getenv('API_TOKEN')  # Get token from environment variable
 BASE_URL = 'https://persona-sound.data.gamania.com/api/v1/public/voice'  # use UAT if testing
 TEXT = '七連敗怎麼不找找自己的問題？你買不起房，找自己的問題好不好？你結不起婚，也找自己的問題好不好？你買不起車，也找自己的問題好不好？你大學畢業找不到工作，也找你自己的問題好不好？為什麼別人就能欺負你？為什麼就你每天上十幾個小時的班？為什麼你的月收入就只有兩三千塊？全部找自己的問題好不好？為什麼你工作一小時，肉蛋奶米麵蔬菜全都買不起？找自己的問題好不好？什麼都讓你找自己的問題！上不去分也找自己的問題？'  # The text you want to synthesize
 MODEL_ID = 2  # Changed from 1 to 2 (max)
